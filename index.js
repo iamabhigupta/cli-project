1293dff667b377f46f982c236283518499c784ab
iamabhigupta <abhishekgupta.july@gmail.com>
2024-07-08 19:57:05 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+// const yargs = require('yargs');
+// const { argv } = yargs(process.argv);
+
+const inquirer = require('inquirer');
+
+const printFiveMoves = async (pokemonName) => {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+  const pokemon = await res.json();
+  const moves = pokemon.moves.map(({ move }) => move.name);
+  console.log(moves.slice(0, 5));
+};
+
+const prompt = inquirer.createPromptModule();
+prompt([
+  {
+    type: 'input',
+    name: 'pokemon',
+    message: 'Enter a pokemon name to view its first 5 moves',
+  },
+]).then((answers) => {
+  const pokemon = answers.pokemon;
+
+  printFiveMoves(pokemon);
+});
